a594ebeb9b8c4c08feb3579a95054c47b1fa246b
Phil Berex <pvn@xloron.com>
2025-07-05 12:58:24 +0200

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -101,14 +101,7 @@ const config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/xloron/stream-slugger-docs',
-            },
+            
           ],
         },
       ],
